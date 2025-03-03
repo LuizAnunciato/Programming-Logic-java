03903a87fd404de9095a0f799acb1f8ca560a650
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 21:53:12 -0300

    Update 002.java

--- 002.java
+++ 002.java
@@ -1,5 +1,5 @@
 """
-Exercício 2 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+Exercício 2 - Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo.
 """
 
 import java.util.Scanner;
