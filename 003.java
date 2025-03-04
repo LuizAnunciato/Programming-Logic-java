b9f9e463ab25b6022978866e954841d219777f11
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-03 21:31:42 -0300

    Update 003.java

--- 003.java
+++ 003.java
@@ -2,5 +2,35 @@
 Exercício 3 - Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem iguais, deverá somar os dois valores
 """
 
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int soma = 0;
+        int multi = 0;
+        int C = 0;
+
+        System.out.println("Digite o valor de A: ");
+        int A = sc.nextInt();
+
+        System.out.println("Digite o valor de B: ");
+        int B = sc.nextInt();
+        
+        if (A == B) {
+            soma = (A+B); 
+            C = soma;           
+        } else {
+            multi = (A*B);
+            C = multi;
+        }
+
+        System.out.printf("O valor de C é: %d",C);
+
+        sc.close();
+
+    }
+}
 
 
