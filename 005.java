96265f32ea0bcf19fa4774e76848b6f4f2a13436
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-03 23:39:13 -0300

    Update 005.java

--- 005.java
+++ 005.java
@@ -1,5 +1,7 @@
 """
-Exercício 5 - Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem iguais, deverá somar os dois valores
+Exercício 5 - Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse 
+
+usuário ganha e imprima na tela o resultado. (Base para o Salário mínimo R$ 1.293,20).
 """
 
 import java.util.Scanner;
