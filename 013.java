c8a9ab096894185e78b0db3a90371fe49d0fe119
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-04 15:27:51 -0300

    Update 013.java

--- 013.java
+++ 013.java
@@ -1,4 +1,26 @@
 """
-Exercício 13 - 
+Exercício 13 - Faça algoritmo que leia o nome e a idade de uma pessoa e imprima na tela o nome da pessoa e se ela é maior ou menor de idade. 
 """
 
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite seu nome: "); 
+    String nome = sc.nextLine();
+
+    System.out.println("Digite sua idade: ");
+    int idade = sc.nextInt();
+
+    if (idade >= 18) {
+        System.out.printf("%s você é maior de idade",nome);
+    }else {
+        System.out.printf("%s você é menor de idade",nome);
+    }
+
+    sc.close();
+    }
+}
