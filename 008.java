25f361d2d742abfbb747b266d9636d928121496a
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-03 23:21:22 -0300

    Update 008.java

--- 008.java
+++ 008.java
@@ -14,11 +14,25 @@ public class App {
         System.out.println("Digite sua altura (Ex: 1,80): ");
         double altura = sc.nextDouble();
 
-        Math.pow(altura,2);
-
         double imc = (peso / Math.pow(altura,2));
 
-        System.out.printf("Seu IMC é: %f", imc);
+        if (imc < 18.5) {
+            System.out.printf("Seu IMC é de: %.2f e você está abaixo do peso",imc);
+        } else if (imc >= 18.6 && imc <= 24.90) {
+            System.out.printf("Seu IMC é de: %.2f e você está no peso ideal, parabéns!",imc);
+        } else if (imc >= 25.0 && imc <= 29.90) {
+            System.out.printf("Seu IMC é de %.2f e você está levemente acima do peso",imc);
+        } else if (imc >= 30.0 && imc <= 34.90) {
+            System.out.printf("Seu IMC é de: %.2f e você está com obesidade grau I",imc);
+        } else if (imc >= 35.0 && imc <= 39.90) {
+            System.out.printf("Seu IMC é de: %.2f e você está com obesidade grau II",imc);
+        } else {
+            System.out.printf("Seu IMC é de: %.2f e você está com obesidade grau III (Mórbida)",imc);
+        }
+        
         sc.close();
+    }
+}
+
     }
 }
