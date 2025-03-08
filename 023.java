5ec2426fa504576dc36d988aa2cab7b9cf6a88fc
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-08 02:30:32 -0300

    Update 023.java

--- 023.java
+++ 023.java
@@ -1,2 +1,24 @@
 """
 Exercício 23 - Criar um programa que leia pelo teclado um valor inteiro e diga se é par ou ímpar.
+"""
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite um número: ");
+    double numero = sc.nextDouble(); 
+
+    if (numero % 2 == 0) {
+        System.out.printf("%.0f é um número par",numero);
+    }else if (numero % 2 != 0) {
+        System.out.printf("%.0f é um número ímpar",numero);
+    }else {
+        System.out.println("Valor inválido.");
+    }
+
+    sc.close();
+    }
+}
