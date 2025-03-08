cedac89eb16a622a54d5a9c938aab64a57760af8
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-07 21:46:15 -0300

    Update 018.java

--- 018.java
+++ 018.java
@@ -1,3 +1,15 @@
 """
 Exercício 18 - Faça um algoritmo que mostre um valor aleatório entre 0 e 100
 """
+
+import java.util.Random;
+
+public class estudo {
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+        int numero = random.nextInt(101);
+        
+        System.out.println("Seu número é: " + numero);
+    }
+}
