549ae0b77909ae72414c82c8d424eda8e68766ef
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-04-19 00:18:35 -0300

    Create Exercício 6 - Faculdade (Prática) - Proprietario.java

---
+++ Exercício 6 - Faculdade (Prática) - Proprietario.java
@@ -0,0 +1,45 @@
+public class Proprietario {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private Endereco endereco;
+
+    public Proprietario(String nome, String cpf, String telefone, Endereco endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
